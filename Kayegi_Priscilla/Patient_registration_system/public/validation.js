b7efbbe7c4953abec34b4eda784df6f359de42be
Priscilla Kayegi <p.kayegi@student.ciu.ac.ug>
2021-08-03 16:18:51 +0300

    adds changes

--- Kayegi_Priscilla/Patient_registration_system/public/validation.js
+++ Kayegi_Priscilla/Patient_registration_system/public/validation.js
@@ -1,8 +1,11 @@
 let validation =()=>{
     let Surname = document.washer.surname;
+    let givenName = document.washer.givenname;
+    
 
     //Errors
     let SurnameError = document.getElementById('surnameError');
+    let givenNameError = document.getElementById('givennameError');
 
     //Regular expressions
     var NameReg = /^[A-Z]([a-z])+$/;
@@ -23,5 +26,24 @@ let validation =()=>{
         return false;
     } else if (Surname.value.length = 0) {
         SurnameError.innerHTML = ''
+    }
+    //Given name validation
+    if (givenName.value.length < 8) {
+        givenNameError.innerHTML = ' Name Should be atleast 8 Characters ';
+        givenNameError.style = 'color:red; font-size:10px;'
+        givenName.style.border = "1px solid red"
+        return false;
+    } else if (givenName.value.length >= 8 && givenName.value.match(NameReg)) {
+        givenNameError.innerHTML = '';
+        givenName.style.border = "1px solid green"
+    } else if (!(givenName.value.length <= 8 && givenName.value.match(NameReg))) {
+        givenNameError.innerHTML = 'Name start with Capital letter ';
+        givenNameError.style = 'color:red; font-size:10px;'
+        givenName.style.border = "1px solid red"
+        return false;
+    } else if (givenName.value.length = 0) {
+        givenNameError.innerHTML = 'Last name must be filled';
+        givenName.style.border = "1px solid red"
+
     }
 }
